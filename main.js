7961547e3876012a00f63e5b55986ee2e9bd0163
Andela Stevanovic <andjela.s166@gmail.com>
2021-12-27 17:44:27 -0500

    commit tree 4f61154182e5039a2bb4cb349e79090fbc8860a2

--- main.js
+++ main.js
@@ -3,11 +3,45 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const hearts = document.querySelectorAll(".like-glyph");
+
+//adding event listener for every click
+for(const glyph of hearts) {
+glyph.addEventListener("click", likeCallBack)
+}
+
+function likeCallback(event){
+  const heart = event.target
+  //invoke () to simulate making a server request
+  mimicServerCall()
+  .then(() => {
+    //response what happens when you click
+    if(heart.innerText === EMPTY_HEART){
+    //adding class to make heart appear red
+    heart.className = "activated-heart"
+    heart.innerText = FULL_HEART;
+    } else{ // when user clicks on full heart , change the heart back to empty
+            //and remove class .activated-heart
+      heart.innerText = EMPTY_HEART;
+      heart.className = ''
+    }
+  }) //when server returns failure we respond with .catch()
+  .catch((error) => {
+    //display the error modal
+    const modal = document.getElementById('modal')
+    //by removing the class name
+    modal.className = ''
+    //server error message
+    modal.innerText = error 
+    setTimeout(function(){
+      modal.className = "hidden"
+    }, 3000)
+  })
+
+}
 
 
-
-
-//------------------------------------------------------------------------------
+  //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
